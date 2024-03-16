10584bbfb588b20af9366cb0fafc7f146b06990f
arifzaidpp <arifzaidaiju@gmail.com>
2024-03-16 23:15:41 +0530

    very basic OLX webpage work complated

--- src/App.js
+++ src/App.js
@@ -9,32 +9,39 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SignupPage from './Pages/Signup';
 import Login from './Pages/Login'
 import Create from './Pages/Create'
+import View from './Pages/ViewPost'
 import { AuthContext, FirebaseContext } from './store/Context';
+import Post from './store/PostContext';
 
 function App() {
-  const {user,setUser} = useContext(AuthContext)
-  const {firebase} = useContext(FirebaseContext)
+  const { user, setUser } = useContext(AuthContext)
+  const { firebase } = useContext(FirebaseContext)
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user)=>{
+    firebase.auth().onAuthStateChanged((user) => {
       setUser(user)
     })
   })
   return (
     <div>
-      <Router>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/signup'>
-          <SignupPage/>
-        </Route>
-        <Route path='/login'>
-          <Login/>
-        </Route>
-        <Route exact path='/create'>
-          <Create />
-        </Route>
-      </Router>
+      <Post>
+        <Router>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/signup'>
+            <SignupPage />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/create'>
+            <Create />
+          </Route>
+          <Route exact path='/view'>
+            <View />
+          </Route>
+        </Router>
+      </Post>
     </div>
   );
 }
--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import './View.css';
+import { PostContext } from '../../store/PostContext';
+import { FirebaseContext } from '../../store/Context';
 function View() {
+  const [userDetails, setUserDetails] = useState()
+  const { postDetails } = useContext(PostContext)
+  const { firebase } = useContext(FirebaseContext)
+
+  useEffect(() => {
+    const { userId } = postDetails
+    firebase.firestore().collection('user').where('id', '==', userId).get().then((res) => {
+      res.forEach(doc => {
+        setUserDetails(doc.data())
+      })
+    })
+  }, [])
+
   return (
     <div className="viewParentDiv">
       <div className="imageShowDiv">
         <img
-          src="../../../Images/R15V3.jpg"
-          alt=""
+          src={postDetails.url}
+          alt="image"
         />
       </div>
       <div className="rightSection">
         <div className="productDetails">
-          <p>&#x20B9; 250000 </p>
+          <p>&#x20B9; {postDetails.price} </p>
           <span>YAMAHA R15V3</span>
           <p>Two Wheeler</p>
           <span>Tue May 04 2021</span>
         </div>
-        <div className="contactDetails">
+        {userDetails && <div className="contactDetails">
           <p>Seller details</p>
-          <p>No name</p>
-          <p>1234567890</p>
+          <p>{userDetails.username}</p>
+          <p>{userDetails.phone}</p>
         </div>
+        }
       </div>
     </div>
   );
